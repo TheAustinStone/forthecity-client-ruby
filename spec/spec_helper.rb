6c272c6f1dce7cddbf908efcf7506924b5f9cd2e
Joseph Simmons <josephdsimmons@gmail.com>
2016-05-19 16:46:25 -0500

    Adds client initialization to spec_helper [#119915797]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,15 @@ require 'unit_helper'
 require 'webmock/rspec'
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    CLIENT = RestoreStrategies::Client.new(
+      ENV['TOKEN'],
+      ENV['SECRET'],
+      ENV['HOST'],
+      ENV['PORT']
+    )
+  end
+
     config.mock_with :rspec do |mocks|
         mocks.verify_doubled_constant_names = true
     end
