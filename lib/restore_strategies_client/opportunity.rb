480786dacfbe2e76d4bef9cbf8df363df0e63f2c
Joseph Simmons <josephdsimmons@gmail.com>
2016-05-19 09:53:36 -0500

    Adjusts opportunity.rb & phone_validator.rb based on Rubocop [#119680329]

--- lib/restore_strategies_client/opportunity.rb
+++ lib/restore_strategies_client/opportunity.rb
@@ -1,44 +1,43 @@
 require_relative 'signup'
 
 module RestoreStrategiesClient
+  # Objectification of the API's opportunity
   class Opportunity
     include ActiveModel::Model
     include ActiveModel::Validations
     include ActiveModel::Conversion
 
     attr_reader :raw, :client, :id, :name, :type, :featured, :description,
-    :location, :items_committed, :items_given, :max_items_needed, :ongoing,
-    :organization, :instructions, :gift_question, :days, :group_type, :issues,
-    :region, :supplies, :skills
+                :location, :items_committed, :items_given, :max_items_needed,
+                :ongoing, :organization, :instructions, :gift_question, :days,
+                :group_type, :issues, :region, :supplies, :skills
 
     def initialize(json_obj, json_str, client)
       @raw = json_str
       @client = client
-      #TODO: Find a better way of capturing the ID
-      @id = json_obj["href"][/[^(\/api\/opportunities\/)]./].to_i
+      # TODO: Find a better way of capturing the ID
+      @id = json_obj['href'][%r{[^(\/api\/opportunities\/)].}].to_i
 
-      json_obj["data"].each do |datum|
-        name = datum["name"].underscore
-        value = datum["value"]
-        instance_variable_set("@#{name}", value)
+      json_obj['data'].each do |datum|
+        instance_variable_set("@#{datum['name'].underscore}", datum['value'])
       end
 
-      json_obj["links"].each do |link|
-        @signup_href = if link["rel"] == "signup" then link["href"] end
+      json_obj['links'].each do |link|
+        @signup_href = link['href'] if link['rel'] == 'signup'
       end
     end
 
     def get_signup
       signup_str = client.get_signup id
-      signup_obj = JSON.parse(signup_str)
       RestoreStrategiesClient::Signup.new(signup_str, self, client, id)
     end
 
     def submit_signup(signup)
       raise TypeError unless signup.is_a? Signup
       raise SignupValidationError,
-        'Signup does not contain valid data' unless signup.valid?
-      client.submit_signup self.id, signup.to_payload
+            'Signup does not contain valid data' unless signup.valid?
+
+      client.submit_signup(id, signup.to_payload)
     end
   end
 end
--- lib/validators/phone_validator.rb
+++ lib/validators/phone_validator.rb
@@ -1,10 +1,11 @@
 require 'phoner'
 
+# Validate phone numbers
 class PhoneValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      unless Phoner::Phone.valid? value, :country_code => '1'
-        record.errors[attribute] << (options[:message] || "is not a valid phone
-        number")
-      end
+  def validate_each(record, attribute, value)
+    unless Phoner::Phone.valid? value, country_code: '1'
+      record.errors[attribute] << (options[:message] ||
+                                   'is not a valid phone number')
     end
+  end
 end
