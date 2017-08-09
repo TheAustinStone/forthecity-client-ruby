4688bcb2aa9c34e44f63d1c97840fdde5391ef1f
Joseph Simmons <josephdsimmons@gmail.com>
2017-08-09 16:51:13 -0500

    Adds FactoryGirl

--- RestoreStrategies.gemspec
+++ RestoreStrategies.gemspec
@@ -30,5 +30,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'rubocop', '0.37.2'
+  spec.add_development_dependency 'faker', '~> 1.8.4'
+  spec.add_development_dependency 'factory_girl', '~> 4.8.0'
   spec.add_development_dependency 'rubocop-rspec'
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'faker'
+
+church_names = [
+  '1st Baptist', 'Community Bible', 'Bible Church', 'United Methodist',
+  'Cornerstone', 'Covenant', 'Grace', 'Cross', 'Life', 'Mount Zion',
+  'Promise', 'Providence', 'Redeemer', 'St David', 'St John', 'St Paul',
+  'St Peter'
+]
+
+FactoryGirl.define do
+  factory :user, class: RestoreStrategies::User do
+    given_name { Faker::Name.first_name }
+    family_name { Faker::Name.last_name }
+    church { church_names[Random.rand(church_names.length - 1)] }
+    church_size { Random.rand(8000) }
+    website { "http://#{Faker::Internet.domain_name}" }
+    email { Faker::Internet.email }
+    franchise_city { Faker::Address.city }
+    street_address { Faker::Address.street_address }
+    address_locality { Faker::Address.city }
+    address_region { Faker::Address.state }
+    postal_code {  Faker::Address.zip }
+  end
+end
